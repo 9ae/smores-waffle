ddbfda10f6b5ae2be2afbc31eaf811ac17d53c28
Alice <alice@pelotoncycle.com>
2015-01-16 15:45:02 -0500

    venue model working

--- app/controllers/venues.js
+++ app/controllers/venues.js
@@ -9,3 +9,18 @@ exports.list = function(req, res) {
 	}); 
 	//res.send('lsit of venues');
 }
+
+exports.create = function(fs_id, name, tags, response){
+	var newVenue = new Venue({
+		fs_id: fs_id,
+		name: name ,
+		tags: tags
+	});
+	//console.log(newVenue);
+	newVenue.save(function(err){
+		if(err){
+			return console.log(err);
+		}
+		response.json(newVenue);
+	});
+};
--- app/routes/main.js
+++ app/routes/main.js
@@ -3,6 +3,12 @@
 module.exports = function(app){
 	app.get('/venues', venues.list);
 
+	app.post('/venues', function(req, res){
+		var jsonBody = req.body;
+		venues.create(jsonBody['fs_id'], jsonBody['name'], jsonBody['tags'], res);
+		//venues.create('a', 'argo tea', ['tea', 'macarons'], res);
+	});
+
 	app.get('*', function(req, res) {
         res.sendfile('./public/views/index.html'); // load our public/index.html file
     });
